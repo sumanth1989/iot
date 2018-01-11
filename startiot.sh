df45212958212f3de96467f037053516d984bd54
projectbullseye <35322236+projectbullseye@users.noreply.github.com>
2018-01-11 12:01:04 -0600

    Add files via upload

---
+++ startiot.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar iotclient.jar 192.168.1.19 5019  https://www-integration.mapquestapi.com/oauth/token fupmC4fCwrSAaudMcHsg0eoSiwJAdy2D ZVBqFeIuMAB1rNww mapquest 71.115.19.60 2301 true 71.115.19.60 2400 >> iot.log
