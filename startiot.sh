b9398df2260d7d1b1cd3c6c8272051f47f982b8f
projectbullseye <35322236+projectbullseye@users.noreply.github.com>
2018-03-19 21:31:46 -0500

    added user paramter

--- startiot.sh
+++ startiot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar iotclient.jar 192.168.1.19 5019  https://www-integration.mapquestapi.com/oauth/token fupmC4fCwrSAaudMcHsg0eoSiwJAdy2D ZVBqFeIuMAB1rNww mapquest 71.115.19.60 2301 true 71.115.19.60 2400 >> iot.log
+java -jar iotclient.jar 166.153.94.224 5019 https://www.mapquestapi.com/oauth/token RDtHEggbxGd3gWUx7o4sV1ZQxzXvMpYF k2PHauma2cx4llUr mapquest 71.115.19.60 2300 true 71.115.19.60 2400 mizaniot >> iot.log
