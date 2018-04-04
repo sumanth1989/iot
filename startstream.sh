45086a919a9877c75bed5b6ee3fda7a966fac99b
Sumanth Mallya <sumanth.89@gmail.com>
2018-04-04 10:56:54 -0500

    Added execute command

--- startstream.sh
+++ startstream.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup sudo str2str -in serial://ttyACM0:230400:8:N:1#ubx -out tcpsvr://:5019#rtcm3 >> stream.log
+sudo -b nohup ./str2str -in serial://ttyACM0:230400:N:1#ubx -out tcpsvr://:5019#rtcm3 >> stream.log
